8b60cdb6c24bcf98b153bdd408050c9f6b360efc
Tititun <vinniopo@mail.ru>
2022-05-20 01:14:59 +0700

    homework lesson_1

---
+++ lesson_1/urls.py
@@ -0,0 +1,20 @@
+from controllers import render
+
+url_map = {
+    '': (render, 'main.html'),
+    'about': (render, 'about.html'),
+    'not_found': (render, 'not_found.html')
+}
+
+class UrlDispatcher:
+    def __init__(self, url: str, method: str):
+        self.url = url.strip('/')
+        self.method
+
+    def get_page(self):
+        if self.url in url_map:
+            controller, page = url_map[self.url]
+            return controller(page)
+        else:
+            controller, page = url_map['not_found']
+            return controller(page, error=self.url)
