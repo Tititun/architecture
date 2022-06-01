76aeb62829876263c6076b1013c4e5c1b3ca793f
Tititun <vinniopo@mail.ru>
2022-06-01 10:55:11 +0700

    _

--- start_instance.sh
+++ start_instance.sh
@@ -2,7 +2,6 @@ apt update &&
 apt install -y nginx &&
 apt install -y firewalld &&
 apt install -y pip &&
-pip install Jinja2 &&
-pip install gunicorn &&
 git clone https://github.com/Tititun/architecture.git &&
+pip install -r architecture/requirements.txt &&
 sudo firewall-cmd --permanent --add-service=http && firewall-cmd --reload
