c9775505259e2fccde3d8a0b93dbc4e340bbcce4
Tititun <vinniopo@mail.ru>
2022-06-08 01:57:33 +0700

    added logging framework and fake framework

--- lesson_5/framework/wsgi.py
+++ lesson_5/framework/wsgi.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 from .request import Request
 from .response import Response
 from .view import View
@@ -5,6 +6,7 @@ from .logger import Logger
 from.url import Url
 
 logger = Logger('wsgi')
+
 
 class Framework:
     def __init__(self, urls):
@@ -33,3 +35,18 @@ class Framework:
         if hasattr(view, request.method):
             return getattr(view, request.method)(view, request)
         return Response('400 ERROR', 'method not supported')
+
+
+class LoggingFramework(Framework):
+    def get_view(self, request: Request):
+        print('Request method:', request.method)
+        print('Request parameters:', request.query_params)
+        print('Request path:', request.path)
+        print('========================')
+        return super().get_view(request)
+
+
+class FakeFramework(Framework):
+    def __call__(self, environ, start_response):
+        response = Response('200 OK', 'Hello from Fake')
+        return [response.body.encode('utf-8')]
